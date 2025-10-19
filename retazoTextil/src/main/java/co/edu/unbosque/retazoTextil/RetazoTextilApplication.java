964c41f045220e160cb3ee7958ef776778b279dd
SEBASYT523 <albarracinisazajuansebastian@gmail.com>
2025-10-19 13:43:59 -0500

    SPRING BACK
    Este commit se trata sobre el Spring del Back

---
+++ retazoTextil/src/main/java/co/edu/unbosque/retazoTextil/RetazoTextilApplication.java
@@ -0,0 +1,13 @@
+package co.edu.unbosque.retazoTextil;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RetazoTextilApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RetazoTextilApplication.class, args);
+	}
+
+}
---
+++ retazoTextil/src/test/java/co/edu/unbosque/retazoTextil/RetazoTextilApplicationTests.java
@@ -0,0 +1,13 @@
+package co.edu.unbosque.retazoTextil;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RetazoTextilApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
