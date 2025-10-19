877cd27501191b0e18167382bf5a523ce01c710c
Alejandro <ysilvar@unbosque.edu.co>
2025-10-19 18:37:45 -0500

    CAMBIOS EN EL SERVER
    Se realizaron cambios en el server para añadir un comentario.

--- retazoTextil/src/main/java/co/edu/unbosque/retazoTextil/ServletInitializer.java
+++ retazoTextil/src/main/java/co/edu/unbosque/retazoTextil/ServletInitializer.java
@@ -1,5 +1,5 @@
 package co.edu.unbosque.retazoTextil;
-
+//hOLA//
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
